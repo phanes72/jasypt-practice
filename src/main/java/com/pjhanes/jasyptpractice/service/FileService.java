bfc1cb2238666bd25fa4f89c5b8e3b05215a5cef
Patrick Hanes <phanes72@gmail.com>
2021-12-18 11:16:35 -0700

    First Commit

---
+++ src/main/java/com/pjhanes/jasyptpractice/service/FileService.java
@@ -0,0 +1,18 @@
+package com.pjhanes.jasyptpractice.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+@Service("fileService")
+public class FileService {
+
+    @Value("${test.encrypted.pw}")
+    private String pw;
+
+
+    public void displayPW() {
+        System.out.println("This is the pw: " + pw);
+
+    }
+}
---
+++ src/test/java/com/pjhanes/jasyptpractice/JasyptPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjhanes.jasyptpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
