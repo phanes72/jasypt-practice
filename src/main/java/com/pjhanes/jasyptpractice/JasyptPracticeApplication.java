b2e31b32caeb0041e306d2b0f5b95ad3331df2e2
Patrick Hanes <phanes72@gmail.com>
2021-12-18 14:14:53 -0700

    Got it working

--- src/main/java/com/pjhanes/jasyptpractice/JasyptPracticeApplication.java
+++ src/main/java/com/pjhanes/jasyptpractice/JasyptPracticeApplication.java
@@ -2,24 +2,20 @@ package com.pjhanes.jasyptpractice;
 
 import com.pjhanes.jasyptpractice.configuration.AppConfig;
 import com.pjhanes.jasyptpractice.service.FileService;
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.core.env.Environment;
 
-@EnableEncryptableProperties
+
 @SpringBootApplication
 public class JasyptPracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JasyptPracticeApplication.class, args);
-		AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		FileService fileService = (FileService) context.getBean("fileService");
-		fileService.displayPW();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptPracticeApplication.class, args);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        FileService fileService = (FileService) context.getBean("fileService");
+        fileService.displayPW();
+    }
 
 }
--- src/main/java/com/pjhanes/jasyptpractice/configuration/AppConfig.java
+++ src/main/java/com/pjhanes/jasyptpractice/configuration/AppConfig.java
@@ -1,5 +1,6 @@
 package com.pjhanes.jasyptpractice.configuration;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@EnableEncryptableProperties
 @ComponentScan(basePackages = "com.pjhanes.jasyptpractice")
 @PropertySource(value = {"classpath:application.properties"})
 public class AppConfig {
